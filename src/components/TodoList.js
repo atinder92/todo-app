adbe637cce4c7d59f3e1d1dd104ee16b66eb0d20
Atinder Singh <atinder.s.92@gmail.com>
2022-01-10 22:38:06 +0530

    added ToggleButtonGroup

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Event as EventIcon } from "@mui/icons-material";
 import TodoListItem from "./TodoListItem";
-import { Alert } from "@mui/material";
+import { Alert, ToggleButtonGroup, ToggleButton } from "@mui/material";
 const TodoList = (props) => {
+  const [todoView, setTodoView] = useState("list");
+
+  const selectViewHandler = (_, view) => {
+    setTodoView(view);
+  };
   return (
     <div className="todo-list-container">
       <h2>
         <EventIcon /> What's upcoming
       </h2>
-      <>
-        {props.todos.map((todo) => {
-          return <TodoListItem todo={todo} key={todo._id} />;
-        })}
-        {props.todos.length === 0 && (
-          <Alert severity="info">
-            Nothing to display! Please create some todo items.
-          </Alert>
-        )}
-      </>
+      <div>
+        <ToggleButtonGroup
+          color="primary"
+          exclusive
+          onChange={selectViewHandler}
+        >
+          <ToggleButton value="list">List</ToggleButton>
+          <ToggleButton value="calendar">Calendar</ToggleButton>
+        </ToggleButtonGroup>
+      </div>
+      {todoView === "list" ? (
+        <div>
+          {props.todos.map((todo) => {
+            return <TodoListItem todo={todo} key={todo._id} />;
+          })}
+          {props.todos.length === 0 && (
+            <Alert severity="info">
+              Nothing to display! Please create some todo items.
+            </Alert>
+          )}
+        </div>
+      ) : (
+        <div>
+          <h3>Calendar View</h3>
+        </div>
+      )}
     </div>
   );
 };
