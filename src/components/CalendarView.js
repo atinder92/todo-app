f1292293d5c542b09f80deeef46e62ec9b9f3d2c
Atinder Singh <atinder.s.92@gmail.com>
2022-01-21 20:24:36 +0530

    display event for calendar

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
 import { Calendar, dateFnsLocalizer, Views } from "react-big-calendar";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+} from "@mui/material";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
@@ -18,26 +25,56 @@ const localizer = dateFnsLocalizer({
 });
 
 const CalendarView = ({ todos }) => {
+  const [eventTitle, setEventTitle] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
+  const [openEvent, setOpenEvent] = useState(false);
+
   let events = todos.map((todo) => {
-      return {
-          title: todo.title,
-          start: new Date(todo.dueDate),
-          end: new Date(todo.dueDate)
-      }
+    return {
+      title: todo.title,
+      description: todo.description,
+      start: new Date(todo.dueDate),
+      end: new Date(todo.dueDate),
+    };
   });
+  const selectEventHandler = (slot) => {
+    setEventTitle(slot.title);
+    setEventDescription(slot.description);
+    setOpenEvent(true);
+  };
+
   return (
-    <div>
+    <div className="calendar-view">
+      <Dialog
+        open={openEvent}
+        onClose={() => setOpenEvent(false)}
+        maxWidth="sm"
+        fullWidth={true}
+      >
+        <DialogTitle
+          id="alert-dialog-title"
+          className="calendar-view-dialog-title"
+        >
+          {eventTitle}
+        </DialogTitle>
+        <DialogContent className="calendar-view-dialog-content">
+          <DialogContentText id="alert-dialog-description">
+            {eventDescription}
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
       <Calendar
-        className="calendar-view"
         localizer={localizer}
         defaultView="month"
-        view={Views.MONTH}
-        onView={() => {}}
         startAccessor="start"
         endAccessor="end"
         events={events}
+        popup={true}
+        view={Views.MONTH}
+        onView={() => {}}
+        onSelectEvent={selectEventHandler}
+        selectable={true}
         style={{ height: 500 }}
-        selectable={false}
       />
     </div>
   );
